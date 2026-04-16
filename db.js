5c52e96b0895326cd83da875846eb6b8c6c09e63
Alejandro <alejandro.cuabe@gmail.com>
2026-04-15 20:42:04 -0600

    Primer Commit: Backend listo para render

---
+++ db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    // REQUISITO PARA AIVEN: Configuración de SSL
+    // rejectUnauthorized: false permite conectar sin necesidad de descargar el archivo .pem
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos: ', err);
+        return;
+    }
+    console.log('Conectado exitosamente a la base de datos de Aiven (MySQL)');
+});
+
+module.exports = connection;
